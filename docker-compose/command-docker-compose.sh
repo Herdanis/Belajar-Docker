908baaf8d63b553cd9b69b230198a83bc0be882a
Herdanis <herdanis.pradana@gmail.com>
2022-12-18 21:22:06 +0700

    onprogress docker-compose

---
+++ docker-compose/command-docker-compose.sh
@@ -0,0 +1,14 @@
+# start the docker composeri in config yaml file
+docker compose start
+
+# show container in config yaml file
+docker compose ps
+
+# stop the docker compose in config yaml file
+docker compose stop
+
+# delete/remove all(container, volume, networks) in config yaml file 
+docker compose down
+
+# to show all docker compose project
+docker compose ls 
