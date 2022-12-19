d97593a041777a9a77be4eafd5b1458875e4d2e6
Herdanis <herdanis.pradana@gmail.com>
2022-12-19 17:16:45 +0700

    finish  docker-compose

--- docker-compose/command-docker-compose.sh
+++ docker-compose/command-docker-compose.sh
@@ -12,3 +12,10 @@ docker compose down
 
 # to show all docker compose project
 docker compose ls 
+
+# show container event in realtime 
+docker events
+docker events --filter'<key>=<value>'
+
+# run docker compose with extend service
+docker compose -f docker-compose.yaml -f <filename>.yaml <dockercommand>
